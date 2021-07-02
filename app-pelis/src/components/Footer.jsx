ebca5dc89e441d09feefb2d7935fcbb7f817c69b
Esteban Francou <hola@estebanfrancou.com.ar>
2021-07-02 20:25:36 -0300

    pre-final

---
+++ app-pelis/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+    return (
+        <div className="footer-cont">
+            <p className="footer-text"> Hecho por Esteban con Tutorial de YT</p>
+        </div>
+    )
+}
+
+export default Footer
